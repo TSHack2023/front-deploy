6be876df7ae25e0def8f4db02533f135c141f668
tokun4869 <ba.tokun4869@gmail.com>
2023-08-12 02:55:57 +0000

    build

--- src/App.tsx
+++ src/App.tsx
@@ -10,35 +10,35 @@ import Review from "./pages/Review";
 
 const App = (): JSX.Element => {
   const id = sessionStorage.getItem("id");
-  const homeURL = process.env.PUBLIC_URL;
+  const homeURL = process.env.PUBLIC_URL ?? "";
 
   if (id !== null) {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/Signin">
-            <Redirect to="/Home" />
+          <Route path={homeURL + "/Signin"}>
+            <Redirect to={homeURL + "/Home"} />
           </Route>
-          <Route path="/Signup">
-            <Redirect to="/Home" />
+          <Route path={homeURL + "/Signup"}>
+            <Redirect to={homeURL + "/Home"} />
           </Route>
-          <Route path="/Home/:fileinfo">
+          <Route path={homeURL + "/Home/:fileinfo"}>
             <HomeDetail />
           </Route>
-          <Route path="/Home">
+          <Route path={homeURL + "/Home"}>
             <Home />
           </Route>
-          <Route path="/Contribute">
+          <Route path={homeURL + "/Contribute"}>
             <Contribute />
           </Route>
-          <Route path="/Review/:fileid">
+          <Route path={homeURL + "/Review/:fileid"}>
             <ReviewDetail />
           </Route>
-          <Route path="/Review">
+          <Route path={homeURL + "/Review"}>
             <Review />
           </Route>
-          <Route path="/">
-            <Redirect to="/Home" />
+          <Route path={homeURL + "/"}>
+            <Redirect to={homeURL + "/Home"} />
           </Route>
         </Switch>
       </BrowserRouter>
@@ -47,29 +47,29 @@ const App = (): JSX.Element => {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/Signin">
+          <Route path={homeURL + "/Signin"}>
             <Signin />
           </Route>
-          <Route path="/Signup">
+          <Route path={homeURL + "/Signup"}>
             <Signup />
           </Route>
-          <Route path="/Home/detail">
-            <Redirect to="/Signin" />
+          <Route path={homeURL + "/Home/detail"}>
+            <Redirect to={homeURL + "/Signin"} />
           </Route>
-          <Route path="/Home">
-            <Redirect to="/Signin" />
+          <Route path={homeURL + "/Home"}>
+            <Redirect to={homeURL + "/Signin"} />
           </Route>
-          <Route path="/Contribute">
-            <Redirect to="/Signin" />
+          <Route path={homeURL + "/Contribute"}>
+            <Redirect to={homeURL + "/Signin"} />
           </Route>
-          <Route path="/Review/:fileid">
-            <Redirect to="/Signin" />
+          <Route path={homeURL + "/Review/:fileid"}>
+            <Redirect to={homeURL + "/Signin"} />
           </Route>
-          <Route path="/Review">
-            <Redirect to="/Signin" />
+          <Route path={homeURL + "/Review"}>
+            <Redirect to={homeURL + "/Signin"} />
           </Route>
-          <Route path="/">
-            <Redirect to="/Signin" />
+          <Route path={homeURL + "/"}>
+            <Redirect to={homeURL + "/Signin"} />
           </Route>
         </Switch>
       </BrowserRouter>
